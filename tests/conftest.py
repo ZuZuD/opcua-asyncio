66c73c6f7c52b0555cc0a2085eb72ecb378118c7
Julien Prigent <julienprigent@wanadoo.fr>
2020-10-28 06:39:34 -0700

    [HaClient] for asyncua.
    It exposes an interface similar to Client. You'll find some examples
    and tests to guide you through this new feature.
    
    Under the hood, it starts a keepalive task to monitor the status and
    service level for each server.
    
    It also manages an HaManager task which promotes a primary and
    reconnect unhealthy clients based on the socket status and
    keepalive data collected.
    
    The requests received by users as well as the HaManager decisions
    produce an ideal state called the "ideal_map". Regularly, the reconciliator task
    kicks in, lock this configuration, and applies this ideal configuration.
    It then stores the actual subscriptions status into the "real_map" using the
    lower level components of the library.
    
    Current limits:
    
    - We only support HA WARM mode and datachange notifications (no event/status_change).
    _ We support multiple subscriptions, but a node should only be subscribed once.
    
    Component details:
    
    - HaClient:
        - Configuration based on dataclasses: HaConfig (required) and HaConfigSecurity (optional)
        - Responsible to start up sides tasks: keepalive, hamanager, reconciliator
        - Mutates the ideal_map via VirtualSubscription
        - Generic hooks (i.e: to monitor subscription performance)
    
    - KeepAlive (task):
        - Regularly hits the server to check its service_level / status.
    
    - HaManager (task):
        - Promote primary / secondaries
        - Reconnect disconnected/unhealthy client based on the keepalive
        feedback.
    
    - Reconciliator (task):
        - Applies the ideal configuration
        - Perform checks on the call responses
        - Mutates the real_map
    
    - VirtualSubscription:
        - Key component of the ideal and real maps.
        - Exposes an interface similar to the subscription but only
        store the settings.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,29 @@
 import asyncio
 import pytest
+import operator
+import os
+import socket
+
 from collections import namedtuple
 from asyncua import ua, Client, Server
+from asyncua.client.ha.ha_client import HaClient, HaConfig, HaMode
 from asyncua.server.history import HistoryDict
 from asyncua.server.history_sql import HistorySQLite
 
 from .test_common import add_server_methods
 from .util_enum_struct import add_server_custom_enum_struct
 from threading import Thread
+
+from asyncio import get_event_loop_policy
+from contextlib import closing
+
+import pytest
+from asyncua import Server, ua
+
+
+
+RETRY = 20
+SLEEP = 0.2
 
 port_num = 48540
 port_num1 = 48510
@@ -221,3 +237,191 @@ async def history_server(request):
         srv = await create_history_server(sqlite=True)
         yield srv
         await srv.srv.stop()
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+def retrieve_srv_port(srv) -> str:
+    return srv.endpoint.netloc.split(":")[-1]
+
+
+@pytest.fixture(scope="session")
+def client_key_and_cert(request):
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    cert_dir = os.path.join(base_dir, "examples/certificates") + os.sep
+    key = f"{cert_dir}peer-private-key-example-1.pem"
+    cert = f"{cert_dir}peer-certificate-example-1.der"
+    return key, cert
+
+
+@pytest.fixture(scope="session")
+def server_key_and_cert(request):
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    cert_dir = os.path.join(base_dir, "examples") + os.sep
+    key = f"{cert_dir}private-key-example.pem"
+    cert = f"{cert_dir}certificate-example.der"
+    return key, cert
+
+
+@pytest.fixture(scope="module")
+def ha_config():
+    """
+    Factory method to return a HaConfig
+    """
+
+    def _ha_config(srv_port1, srv_port2):
+        return HaConfig(
+            HaMode.WARM,
+            keepalive_timer=1,
+            manager_timer=1,
+            reconciliator_timer=1,
+            urls=[
+                f"opc.tcp://127.0.0.1:{srv_port1}",
+                f"opc.tcp://127.0.0.1:{srv_port2}",
+            ],
+            session_timeout=30,
+        )
+
+    return _ha_config
+
+
+@pytest.yield_fixture(scope="module")
+async def ha_servers(server_key_and_cert):
+    # start our own server
+    srvs = []
+    key, cert = server_key_and_cert
+
+    async def create_srv(port, key, cert):
+        srv = Server()
+        srvs.append(srv)
+        await srv.init()
+        await srv.set_application_uri("urn:freeopcua:python:discovery")
+        srv.set_endpoint(f"opc.tcp://127.0.0.1:{port}")
+        srv.set_security_policy(
+            [
+                ua.SecurityPolicyType.NoSecurity,
+                ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
+            ]
+        )
+        await srv.load_certificate(cert)
+        await srv.load_private_key(key)
+        await srv.start()
+        # default the service level to 255 once started
+        slevel = srv.get_node(ua.NodeId(ua.ObjectIds.Server_ServiceLevel))
+        await slevel.write_value(ua.Variant(255, ua.VariantType.Byte))
+        return srv
+
+    srv1 = await create_srv(find_free_port(), key, cert)
+    srv2 = await create_srv(find_free_port(), key, cert)
+    yield srv1, srv2
+    # stop the servers
+    for srv in srvs:
+        await srv.stop()
+
+
+@pytest.fixture(scope="module")
+async def srv_variables(ha_servers):
+    """
+    This fixture returns client like ha_client
+    server within the module scope.
+    """
+    clts = []
+    # add the variables to the client
+    node_to_var = {}
+    for srv in ha_servers:
+        for var in ("V1", "V2"):
+            url = f"{srv.endpoint.scheme}://admin@{srv.endpoint.netloc}"
+            c = Client(url, timeout=10)
+            clts.append(c)
+            await c.connect()
+            values = [1, 2, 3]
+            o = c.nodes.objects
+            node = await o.add_variable(3, f"SubscriptionVariable{var}", values)
+            node_to_var[node] = values
+    yield node_to_var
+    # disconnect admin@clients used to write the custom variables
+    for c in clts:
+        await c.disconnect()
+
+
+@pytest.yield_fixture(scope="function")
+async def ha_client(ha_config, ha_servers):
+    """
+    This fixture returns everytime a new
+    HaClient but configured with the same
+    server within the module scope.
+    """
+    srv1, srv2 = ha_servers
+    srv1_port = retrieve_srv_port(srv1)
+    srv2_port = retrieve_srv_port(srv2)
+
+    ha_config = ha_config(srv1_port, srv2_port)
+    ha_client = HaClient(ha_config)
+    yield ha_client
+    if ha_client.is_running:
+        await ha_client.stop()
+
+
+async def wait_clients_socket(ha_client, state):
+    for client in ha_client.get_clients():
+        for _ in range(RETRY):
+            if client.uaclient.protocol and client.uaclient.protocol.state == state:
+                break
+            await asyncio.sleep(SLEEP)
+        assert client.uaclient.protocol.state == state
+
+
+async def wait_sub_in_real_map(ha_client, sub, negation=False):
+    reconciliator = ha_client.reconciliator
+    oper = operator.not_ if negation else operator.truth
+    for client in ha_client.get_clients():
+        url = client.server_url.geturl()
+        for _ in range(RETRY):
+            if oper(
+                reconciliator.real_map.get(url) and reconciliator.real_map[url].get(sub)
+            ):
+                break
+            await asyncio.sleep(SLEEP)
+        assert oper(reconciliator.real_map[url].get(sub))
+
+
+async def wait_node_in_real_map(ha_client, sub, node_str, negation=False):
+    oper = operator.not_ if negation else operator.truth
+    reconciliator = ha_client.reconciliator
+    for client in ha_client.get_clients():
+        url = client.server_url.geturl()
+        for _ in range(RETRY):
+            # virtual subscription must already exist,
+            if reconciliator.real_map.get(url):
+                vs = reconciliator.real_map[url][sub]
+                if oper(node_str in vs.nodes):
+                    break
+            await asyncio.sleep(SLEEP)
+        assert oper(node_str in vs.nodes)
+
+
+async def wait_mode_in_real_map(ha_client, client, sub, mode, value):
+    reconciliator = ha_client.reconciliator
+    await wait_sub_in_real_map(ha_client, sub)
+    url = client.server_url.geturl()
+    vsub = reconciliator.real_map[url][sub]
+    for _ in range(RETRY):
+        option = getattr(vsub, mode)
+        if option == value:
+            break
+        await asyncio.sleep(SLEEP)
+    assert option == value
+
+
+async def wait_for_status_change(ha_client, client, status):
+    # wait for the KeepAlive task to update its client status
+    srv_info = ha_client.clients[client]
+    for _ in range(RETRY):
+        if srv_info.status == status:
+            break
+        await asyncio.sleep(SLEEP)
+    assert srv_info.status == status
